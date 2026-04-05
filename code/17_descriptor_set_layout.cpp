388fef1520f33a136d4933ba3932061c02919fa0
Fietspompje <74879947+ECHekman@users.noreply.github.com>
2026-04-05 02:07:49 +0200

    Good God

--- code/17_descriptor_set_layout.cpp
+++ code/17_descriptor_set_layout.cpp
@@ -1263,7 +1263,7 @@ private:
 
     VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) {
         for (const auto& availablePresentMode : availablePresentModes) {
-            if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+            if (availablePresentMode == VK_PRESENT_MODE_FIFO_KHR) {
                 return availablePresentMode;
             }
         }
